84215bd0ee3228f75637ea6da6cddffef26f897a
Ionut Calara <ionut.calara@gmail.com>
2017-05-17 17:06:27 +0300

    Added initial version

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# plugin-easy-digital-downloads
-Paylkie Easy Digital Downloads
+# Easy Digital Downloads plugin for Paylike
+
+This plugin is *not* developed or maintained by Paylike but kindly made
+available by a user.
+
+Released under the GPL V3 license: https://opensource.org/licenses/GPL-3.0
+
+
+## Supported Easy Digital Downloads versions
+
+- \>= 2.7.5
+
+## Installation
+
+1.Once you have installed Easy Digital Downloads on your Wordpress setup, follow these simple steps:
+  Signup at (paylike.io) [https://paylike.io] (it’s free)
+  
+  1. Create a live account
+  1. Create an app key for your Easy Digital Downloads website
+  1. Upload the plugin files to the `/wp-content/plugins/edd-paylike` directory.
+  1. Activate the plugin through the 'Plugins' screen in WordPress.
+  1. Insert the app key and your public key in the Payment Gateways settings for the Paylike payment plugin
+  
+
+## Updating settings
+
+Under the Easy Digital Downloads paylike settings, you can:
+ * Update the payment method text in the payment gateways list
+ * Update the title that shows up in the payment popup 
+ * Add test/live keys
+ * Change the capture type (Captured/Preapproved)
+ 
+ 
+ ## Transactions 
+
+ There are three transaction operations which you can do:
+1. Capture - in order to capture a preapproved payment, you can click "Process Preapproved" on the payment history page for that specific order in the WordPress admin.
+2. Void - the only way we void a transaction is when this is preapproved, but not "captured". the button for "Cancel Preapproval" is on payment history page.
+3. Refund - in order to refund, you need to have a order with the status "Completed", move it to "Refunded", check the option that appears below "Refund in Paylike", and click update.
+ 
+  
---
+++ edd-paylike/edd-paylike.js
@@ -0,0 +1,190 @@
+(function ($, window, document, undefined) {
+	'use strict';
+
+	var edd_global_vars;
+	var checkout_modal_shown = false;
+
+	if (!edd_paylike_vars.publishable_key) {
+		alert(edd_paylike_vars.no_key_error);
+	}
+
+	function edd_paylike_get_args() {
+		var email = $('#edd-email').val(),
+			amount = $('.edd_cart_total .edd_cart_amount').data('total');
+
+		if ('true' != edd_paylike_vars.is_zero_decimal) {
+			amount *= 100;
+			amount = Math.round(amount);
+		}
+
+		return {
+			title: edd_paylike_vars.store_name,
+			currency: edd_paylike_vars.currency,
+			amount: amount,
+			locale: edd_paylike_vars.locale,
+			description: edd_paylike_vars.payment_description,
+			custom: {
+				//orderId: '',
+				products: edd_paylike_vars.products,
+				customer: {
+					first_name: $('#edd-first').val(),
+					last_name: $('#edd-last').val(),
+					email: email,
+					ip: edd_paylike_vars.customer_ip
+					//telephone: '',
+					//address: '',
+				},
+				locale: edd_paylike_vars.locale,
+				platform: edd_paylike_vars.platform,
+				platform_version: edd_paylike_vars.platform_version,
+				ecommerce: edd_paylike_vars.ecommerce,
+				version: edd_paylike_vars.version
+			}
+		}
+	}
+
+	function edd_paylike_response_handler(err, res) {
+		if (err) {
+			// re-enable the submit button
+			$('#edd_purchase_form #edd-purchase-button, #edd_profile_editor_form #edd_profile_editor_submit').attr("disabled", false);
+
+			var error = '<div class="edd_errors"><p class="edd_error">' + err.message || err + '</p></div>';
+
+			// show the errors on the form
+			$('#edd-paylike-payment-errors').html(error);
+
+			$('.edd-cart-ajax').hide();
+			if (edd_global_vars.complete_purchase)
+				$('#edd-purchase-button').val(edd_global_vars.complete_purchase);
+			else
+				$('#edd-purchase-button').val('Purchase');
+
+		} else {
+			var form$ = $("#edd_purchase_form, #edd_profile_editor_form");
+
+			var trxid = res.transaction.id;
+			form$.append("<input type='hidden' name='edd_paylike_token' value='" + trxid + "' />");
+
+			// and submit
+			form$.get(0).submit();
+		}
+	}
+
+	function edd_paylike_get_form() {
+		var $form = $('form#edd_purchase_form');
+		$form = $form.length ? $form : $('form#edd_profile_editor_form');
+
+		return $form;
+	}
+
+	function edd_paylike_process_card() {
+		var paylike = Paylike(edd_paylike_vars.publishable_key),
+			args = edd_paylike_get_args(),
+			$form = edd_paylike_get_form();
+
+		// disable the submit button to prevent repeated clicks
+		$('#edd_purchase_form #edd-purchase-button, #edd_profile_editor_form #edd_profile_editor_submit').attr('disabled', 'disabled');
+
+		paylike.pay($form[0], args, edd_paylike_response_handler);
+
+		return false; // submit from callback
+	}
+
+	// ======================================================
+	// Document ready event
+	// ======================================================
+	$(document).ready(function () {
+		var $body = $('body');
+
+		// when we only have a single active gateway, the 'edd_gateway_loaded' is not getting triggered
+		if ($('input[name="edd-gateway"]').val() == 'paylike') {
+			Paylike.assistNumber($('input.card-number')[0]);
+			Paylike.assistExpiry($('input.card-expiry')[0]);
+		}
+		// enable input helper functionality
+		$body.on('edd_gateway_loaded', function (event, payment_mode) {
+			console.log('tet');
+			console.log(payment_mode);
+			if (payment_mode === 'paylike') {
+				Paylike.assistNumber($('input.card-number')[0]);
+				Paylike.assistExpiry($('input.card-expiry')[0]);
+			}
+		});
+
+		// non ajaxed
+		$body.on('click', '#edd_purchase_form input[type="submit"], #edd_profile_editor_form input[type="submit"]', function (event) {
+
+			// copied form edd-ajax.js
+			var eddPurchaseform = document.getElementById('edd_purchase_form');
+			if (typeof eddPurchaseform.checkValidity === "function" && false === eddPurchaseform.checkValidity()) {
+				return;
+			}
+
+			if ($(this).attr('name') === 'edd_login_submit') {
+				return;
+			}
+
+			if (( $('input[name="edd-gateway"]').val() === 'paylike' && $('.edd_cart_total .edd_cart_amount').data('total') > 0 )) {
+
+				$(this).after('<span class="edd-cart-ajax"><i class="edd-icon-spinner edd-icon-spin"></i></span>');
+
+				// stops from triggering edd default callback
+				if (!$('input[name="edd_paylike_token"]').length) {
+					event.stopPropagation();
+				} else {
+					event.preventDefault();
+					return;
+				}
+
+				event.preventDefault();
+
+				if ('true' === edd_paylike_vars.checkout) {
+					if (checkout_modal_shown) {
+						return;
+					}
+
+					checkout_modal_shown = true;
+
+					var paylike = Paylike(edd_paylike_vars.publishable_key);
+					var args = edd_paylike_get_args();
+
+					paylike.popup(args,
+						function (err, res) {
+							var $purchaseBtn = jQuery('#edd-purchase-button');
+							// we need to close no matter what.
+							checkout_modal_shown = false;
+							jQuery('.edd-cart-ajax').hide();
+							$purchaseBtn.prop('disabled', false);
+
+							// if we have an error we bail out
+							if (err && err == 'closed') {
+								return false;
+							} else if (err) {
+								alert(edd_paylike_vars.error_prefix + err.text);
+								return false;
+							}
+
+							var form$ = jQuery("#edd_purchase_form, #edd_profile_editor_form");
+
+							// insert the transaction id into the form so it gets submitted to the server
+							var trxid = res.transaction.id;
+							form$.append("<input type='hidden' name='edd_paylike_token' value='" + trxid + "' />");
+
+							$purchaseBtn.val(edd_paylike_vars.submit_text);
+
+							// and submit
+							var submit_button = form$.find('input[type="submit"][name!="edd_login_submit"]');
+							submit_button.click();
+						}
+					);
+				} else {
+					edd_paylike_process_card();
+				}
+			}
+		});
+	});
+
+})(jQuery, window, document);
+
+
+
